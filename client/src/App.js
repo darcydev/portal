2641a0e00283721dfadbff2c3ebd201c23a86a2d
Darcy Price <darcyelsing@gmail.com>
2020-03-28 09:50:35 +1100

    included basic nav component and react-router-dom setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
-function App() {
+import AuthPage from './pages/Auth';
+import ClientPage from './pages/Client';
+
+import MainNavigation from './components/Navigation/MainNavigation';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <>
+        <MainNavigation />
+        <main>
+          <Switch>
+            <Redirect from='/' to='/auth' exact />
+            <Route path='/auth' component={AuthPage} />
+            <Route path='/clients' component={ClientPage} />
+          </Switch>
+        </main>
+      </>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ client/src/components/Navigation/MainNavigation.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+export default function MainNavigation() {
+  return (
+    <StyledHeader>
+      <StyledLogo>
+        <h1>LOGO</h1>
+      </StyledLogo>
+      <StyledNav>
+        <ul>
+          <li>
+            <NavLink to='/auth'>Authenticate</NavLink>
+          </li>
+        </ul>
+      </StyledNav>
+    </StyledHeader>
+  );
+}
+
+const StyledHeader = styled.header`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 3.5rem;
+  background: #01d1d1;
+  padding: 0 1rem;
+  display: flex;
+  align-items: center;
+`;
+
+const StyledLogo = styled.div`
+  h1 {
+    margin: 0;
+    font-size: 1.5rem;
+  }
+`;
+
+const StyledNav = styled.nav`
+  margin-left: 1.5rem;
+
+  ul {
+    display: flex;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+
+    li {
+      margin: 0 1rem;
+
+      a {
+        text-decoration: none;
+        color: #000;
+
+        :hover,
+        :active {
+          color: #f8e264;
+        }
+      }
+    }
+  }
+`;
